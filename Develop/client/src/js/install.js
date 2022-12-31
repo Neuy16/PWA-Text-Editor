233c0d12bcd5373a5e1ee44eb3f3e0842ab2c7e2
Neuy <neuwirth.andrew9@gmail.com>
2022-12-31 00:17:59 -0500

    wrote package scripts, server starts and serves client

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
+    // Store the triggered events
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    // Removes the hidden class from the button
+butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+    if(!promptEvent) {
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.defferedPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('install hit')
+    window.deferredPrompt = null;
+});
